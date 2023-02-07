c7c6767eaf4d782d5be53cb953c78a53deb48fe1
Andrew <bobrovskyandrew@gmail.com>
2023-02-07 21:12:13 +0200

    Updates

--- src/components/Slider/Slider.styled.js
+++
@@ -1,8 +0,0 @@
-import { styled } from '@mui/material/styles';
-import MuiInput from '@mui/material/Input';
-
-export const Input = styled(MuiInput)`
-  width: 55px;
-  margin-bottom: 30px;
-  margin-left: 20px;
-`;
